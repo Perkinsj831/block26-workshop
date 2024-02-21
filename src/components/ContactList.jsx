b0cc3e2cd35737b795678bd6d9f5da192ab089db
John Perkins <mrgreen@MrGreens-Laptop.local>
2024-02-21 12:27:06 -0600

    added Contanct List

---
+++ src/components/ContactList.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function ContactList() {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Contact List</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Name</td>
+                    <td>Email</td>
+                    <td>Phone</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+
+export default ContactList
